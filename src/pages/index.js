133a78bc4c5ea0d3505d270a8f961f9ec1a5e842
Nadiia Astakhova <nadine.astakhova@gmail.com>
2020-01-20 17:21:03 +0100

    add expirience, other

--- src/pages/index.js
+++ src/pages/index.js
@@ -35,7 +35,7 @@ export default class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <SEO title="Home"/>
+        <SEO title="Nadiia Astakhova"/>
         <section className="page-section  bg-dark" id="about">
           <div className="container">
             <div className="row justify-content-center">
@@ -43,8 +43,20 @@ export default class IndexPage extends React.Component {
                 <h2 className="text-white mt-0">About me</h2>
                 <hr className="divider my-4"/>
                 <p className="text-white-50 mb-4">I am Nadiia. I </p>
-                <a className="btn btn-light btn-xl js-scroll-trigger" href="#services"
-                   onClick={Scroller.handleAnchorScroll}>Get Started!</a>
+
+
+              </div>
+            </div>
+            <hr className="divider my-4"/>
+            <div className="row justify-content-center">
+              <div className="col-md-1 text-center">
+                <a href="https://www.linkedin.com/in/nadiia-astakhova-015547b0" target="_blank">
+                  <i className="fab fa-linkedin fa-3x mb-3 text-muted"></i></a>
+              </div>
+
+              <div className="col-md-1 text-center">
+                <a href="https://github.com/NadineAstakhova" target="_blank">
+                  <i className="fab fa-github-square fa-3x mb-3 text-muted"></i></a>
               </div>
             </div>
           </div>
@@ -57,9 +69,22 @@ export default class IndexPage extends React.Component {
             <div className="row">
               <div className="col-lg-8 col-md-6 text-left">
                 <div className="mt-5">
-                  <h3 className="h4 mb-2">Rockitworks</h3>
-                  <p className="text-muted mb-0">Software Programmer</p>
-                  <p className="text-muted mb-0">Working with internal systems of the company. Working with clients of the firm that include development firms. Participation in different client projects: sites and CMS of online-offline shops, mobile operators etc. My role is a backend developer with the use of: PHP 5.5 - 7.2, Laravel 5.6, Symfony 3.3 - 4.0, Zend Framework 2, MySQL. In some projects, like a full-stack with frontend-end technologies: JavaScript(ECMAScript 6), jQuery, HTML, CSS, Bootstrap. I took part in the integration of online stores with payment systems, migration of projects from old versions of Symfony to new ones, integration of Push notifications (Firebase) with the API of the application, optimization of database queries, integration with payment systems (PayU, eCard) etc.</p>
+                  <h3 className="h4 mb-3">Rockitworks</h3>
+                  <p className="mb-2">Software Programmer</p>
+                  <p className="text-muted mb-0">
+                    Projects: different internal systems and client's applications
+                    (online telecommunications service stores, electronic document management, CMS and API for mobile application of a large supermarket network).
+                    These are mostly loaded websites consisting of CMS and public page, REST-Api applications.
+                    <br/> <br/>
+                    Responsibilities: integration of online stores with payment systems (PayU, eCard); migration of projects from old versions of Symfony to new ones;
+                    integration of Push notifications (Firebase) with the Application API; optimization of database queries;
+                    integration with AWS, GA, ElasticSearch, SMTP, specific APIs for mobile operators etc.
+                    <br/> <br/>
+                    My role is a backend developer with the use of: PHP 5.5 - 7.2, Laravel 5.6, Symfony 3.3 - 4.0, Zend Framework 2, MySQL. In some projects,
+                    like a full-stack with frontend-end technologies: JavaScript(ECMAScript 6), jQuery, HTML, CSS, Bootstrap.
+                    <br/><br/>
+                    Version control systems: GIT (GitLab, GitHub), TortoiseSVN. Project management systems: Jira, Trello, Redmine.
+                  </p>
                 </div>
               </div>
               <div className="col-lg-4 col-md-6 text-right">
@@ -71,10 +96,16 @@ export default class IndexPage extends React.Component {
             <div className="row">
               <div className="col-lg-8 col-md-6 text-left">
                 <div className="mt-5">
-                  <h3 className="h4 mb-2">Donetsk National University</h3>
-                  <p className="text-muted mb-0">IT staff at the Computer Technology Department</p>
-                  <p className="text-muted mb-0">Heads of dean's office interested in my web-system. So, they offered me to finalize it. My responsibilities are all development cycles: architecture, design, code, testing. Used PHP 5, PHPUnit, Yii2, JavaScript, JQuery, Pjax, CSS3, HTML5, Bootstrap3, MySQL
-                    and GIT.</p>
+                  <h3 className="h4 mb-3">Donetsk National University</h3>
+                  <p className="mb-2">IT staff at the Computer Technology Department</p>
+                  <p className="text-muted mb-0">
+                    Responsibilities: development and integration of a remote student lab control system.
+                    <br/> <br/>
+                    Technologies: PHP 5.4, Yii2, JavaScript, jQuery, Pjax, CSS3, HTML5, Bootstrap 3, MySQL, PHPUnit and GIT.
+                    <br/> <br/>
+                    This system was my coursework.
+                    The management of the department offered to finalize the project and to launch it inside the Faculty.
+                    </p>
                 </div>
               </div>
               <div className="col-lg-4 col-md-6 text-right">
@@ -270,41 +301,75 @@ export default class IndexPage extends React.Component {
             <hr className="divider my-4"/>
 
             <div className="row lang-row">
-            <div className="col-lg-2 col-md-6 text-center lang-col">
-              <div className="mt-6">
-                <h3 className="h4 mb-2">Russian</h3>
-                <p className="text-muted mb-0">Native</p>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 text-center lang-col">
-              <div className="mt-6">
-                <h3 className="h4 mb-2">Ukrainian</h3>
-                <p className="text-muted mb-0">Native</p>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 text-center lang-col">
-              <div className="mt-6">
-                <h3 className="h4 mb-2">English</h3>
-                <p className="text-muted mb-0">B1</p>
-              </div>
-            </div>
-            <div className="col-lg-2 col-md-6 text-center lang-col">
-              <div className="mt-6">
-                <h3 className="h4 mb-2">Polish</h3>
-                <p className="text-muted mb-0">B1</p>
-              </div>
-            </div>
               <div className="col-lg-2 col-md-6 text-center lang-col">
                 <div className="mt-6">
-                  <h3 className="h4 mb-2">French</h3>
-                  <p className="text-muted mb-0">A2</p>
+                  <h3 className="h4 mb-2">Russian</h3>
+                  <p className="text-muted mb-0">Native</p>
                 </div>
               </div>
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <h3 className="h4 mb-2">Ukrainian</h3>
+                  <p className="text-muted mb-0">Native</p>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <h3 className="h4 mb-2">English</h3>
+                  <p className="text-muted mb-0">B1</p>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <h3 className="h4 mb-2">Polish</h3>
+                  <p className="text-muted mb-0">B1</p>
+                </div>
+              </div>
+                <div className="col-lg-2 col-md-6 text-center lang-col">
+                  <div className="mt-6">
+                    <h3 className="h4 mb-2">French</h3>
+                    <p className="text-muted mb-0">A2</p>
+                  </div>
+                </div>
             </div>
 
             <hr className="divider my-4"/>
-            <p className="text-muted mb-0">Something</p>
+            <p className="text-muted mb-0">Other skills</p>
             <hr className="divider my-4"/>
+
+            <div className="row lang-row">
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <i className="fab fa-react fa-3x mb-3"></i>
+                  <p className="text-muted mb-0">React.js</p>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <i className="fab fa-angular fa-3x mb-3"></i>
+                  <p className="text-muted mb-0">Angular 1</p>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <i className="fas fa-camera fa-3x mb-3"></i>
+                  <p className="text-muted mb-0">Photography</p>
+                </div>
+              </div>
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <i className="fab fa-adobe fa-3x mb-3"></i>
+                  <p className="text-muted mb-0">Adobe Photoshop</p>
+                </div>
+              </div>
+
+              <div className="col-lg-2 col-md-6 text-center lang-col">
+                <div className="mt-6">
+                  <i className="fas fa-tasks fa-3x mb-3"></i>
+                  <p className="text-muted mb-0">Self-management</p>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
@@ -314,16 +379,17 @@ export default class IndexPage extends React.Component {
               <div className="col-lg-8 text-center">
                 <h2 className="mt-0">CONTACT ME</h2>
                 <hr className="divider my-4"/>
-                <p className="text-muted mb-5">Ready to start your next project with me? Send me an
+                <p className="text-muted mb-5">If you have question or job offer, send me an
                   email
                   and I will get back to you as soon as possible!</p>
               </div>
             </div>
-            <div className="row">
-              <div className="col-lg-4 mr-auto text-center">
+            <div className="row justify-content-center">
+              <div className="col-lg-4 text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
                 <a className="d-block" href="mailto:nadine.astakhova@gmail.com">nadine.astakhova@gmail.com</a>
               </div>
+
             </div>
           </div>
         </section>
