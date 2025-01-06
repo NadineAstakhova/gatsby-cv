d05f3025b7d7afb7fe30386bcde1ccf411bc478c
Nadiia Astakhova <nadine.astakhova@gmail.com>
2025-01-06 22:06:05 +0100

    fix pdf

--- src/components/downloadpdf.js
+++ src/components/downloadpdf.js
@@ -10,7 +10,7 @@ const Download = () => {
       }
     }
   `)
-    const link = data.file.publicURL;
+  const link = "/gatsby-cv" + data.file.publicURL;
     return <a className="btn btn-primary btn-xl" href={link} target="_blank">Download My CV</a>
 }
 
